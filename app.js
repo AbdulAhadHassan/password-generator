06fcd6dd50bd02b7581fea8215469c72d5571f6f
Abdul Ahad Hassan <malikabdulahadhassan@gmail.com>
2024-05-12 20:19:36 +0500

    2.0

--- app.js
+++ app.js
@@ -4,47 +4,75 @@ let generator = document.getElementById("password-generator")
 
 let passwordText = document.getElementById("password-text")
 
-let rangeNum
 
-passwordRange.onmousemove = function() {
+let rangeNum;
+function rangeBtn() {
     rangeNum = passwordRange.value
-    rangeValue.textContent = rangeNum
-}    
+    rangeValue.textContent = "Password Length : " + rangeNum
+    
+    return rangeNum
+} 
 
- function generatePassword(passwordLength, includeUppercase, includeLowercase, includeNumber, includeSpacialSymbols){
+
+function generatePassword(){
+    
+    const Length = rangeNum
+    
+    let match = true
+    
+    const includeUppercase = document.getElementById("upper-case").checked
+    const includeLowercase = document.getElementById("lower-case").checked
+    const includeNumber = document.getElementById("num").checked
+    const includeSpacialSymbols = document.getElementById("spacial-characters").checked
+    
     const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     const lowercaseChars = "abcdefghijklmnopurstuvwxyz"
     const numberChars = "0123456789"
     const spacialSymbolchars = "!@#$%^&*)(+_}{?><"
-
+    
     let allowChars = ''
     let password = ''
-
+    
     allowChars += includeUppercase ? uppercaseChars : ""
     allowChars += includeLowercase ? lowercaseChars : ""
     allowChars += includeNumber ? numberChars : ""
     allowChars += includeSpacialSymbols ? spacialSymbolchars : ""
 
-    if(allowChars.length === 0){
-        return "At least one check box should be checked"
+
+    
+    
+    if( allowChars.length === 0){
+        password = "At least one check box should be checked "
+        match = false
+        
     }
+    
+    // if(length===0 ){
+    //     password = "SELECT LENGHT THEN PRESS GENERATE"
+    //     match = false
+    // }
+    
 
-
-
-    return ''
+    if(match){    
+        // password = ""
+        for( i=0 ; i<Length; i++){
+            let  randomNum = Math.floor(Math.random() * allowChars.length)
+            password += allowChars[randomNum]
+        }
+    }
+    passwordText.textContent = password
+        
+        
+    
 }
 
 
 
 
 // let num = document.getElementById("num")
-const passwordLength = 13 //rangeNum
-const includeUppercase = document.getElementById("upper-case").checked
-const includeLowercase = document.getElementById("lower-case").checked
-const includeNumber = document.getElementById("num").checked
-const includeSpacialSymbols = document.getElementById("spacial-characters").checked
 
-generator.onclick = generatePassword(passwordLength, includeUppercase, includeLowercase, includeNumber, includeSpacialSymbols)
-const password = generatePassword(passwordLength, includeUppercase, includeLowercase, includeNumber, includeSpacialSymbols)
 
-passwordText.textContent = password
+
+// 
+// generator.onclick = password
+//  console.log(password)
